4e3d29d29a14cb811c462f06fe7aa82721d29f18
amitIAI <amit@indivisionai.com>
2025-02-22 13:15:31 +0530

    Update pdf_processing.py

--- pdf_processing.py
+++ pdf_processing.py
@@ -7,7 +7,7 @@ def get_pdf_text(pdf_path):
     text = "\n".join([page.get_text("text") for page in doc])
     return sent_tokenize(text)
 
-def chunk_text(sentences, chunk_size=5000):
+def chunk_text(sentences, chunk_size=1000):
     """ Breaks text into smaller chunks while keeping sentences intact. """
     chunks, current_chunk, current_length = [], [], 0
     for sentence in sentences:
